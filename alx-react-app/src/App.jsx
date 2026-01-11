e0cde1f3e99e781566274a86ba1b7b42ac434c53
nalex1 <alxnkuru@gmail.com>
2026-01-11 21:56:07 +0200

    PROPS PROPERTIES APPS

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 function App() {
   const [count, setCount] = useState(0)
 
@@ -22,6 +23,11 @@ function App() {
         <Header />
         <MainContent />
         <Footer />
+        <UserProfile 
+        name="Alice" 
+        age="25"
+        bio="Loves hiking and photography"
+        />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
