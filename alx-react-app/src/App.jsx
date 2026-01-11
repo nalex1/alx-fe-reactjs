626fb1056fd020e7b9d65677312e42b287d49453
nalex1 <alxnkuru@gmail.com>
2026-01-11 20:43:01 +0200

    ohe

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import WelcomeMessage from './components/WelcomeMessage'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -15,6 +15,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <WelcomeMessage />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
