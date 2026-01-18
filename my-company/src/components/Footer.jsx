21d1b8e1802e2c569ab1010be87b2a4d700e5a57
nalex1 <alxnkuru@gmail.com>
2026-01-19 00:31:33 +0200

    rout app

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        marginTop: "20px",
+        padding: "10px",
+        textAlign: "center",
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      <p>© 2026 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
