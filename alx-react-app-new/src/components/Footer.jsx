4acd86be469c4d001dbb0852e309a408e83521bd
nalex1 <alxnkuru@gmail.com>
2026-01-18 22:48:02 +0200

    style mine

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+    return(
+        <footer style={{ 
+            backgroundColor: 'lightgray', 
+            color: 'red', }}>
+           <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent(){
+    return(
+        <main style={{ 
+            padding: '20px', 
+            fontFamily: 'Arial, sans-serif' }}>
+        <h2 style={{ textDecoration: 'underline' }}>Welcome to My Favorite Cities</h2>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+    );
+}
+export default MainContent;
