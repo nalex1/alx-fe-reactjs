0d4d0087dbaa71746f87795ef4d116c218ae51de
nalex1 <alxnkuru@gmail.com>
2026-01-25 21:54:35 +0200

    stores

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,19 @@
+// RecipeList component
+  import { useRecipeStore } from './stores/recipeStore';
+
+  const RecipeList = () => {
+    const recipes = useRecipeStore(state => state.recipes);
+
+    return (
+      <div>
+        {recipes.map(recipe => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+ 
