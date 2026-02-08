b6feaf17f2ff94d5be5b2881fb0205d0e357b7c2
nalex1 <alxnkuru@gmail.com>
2026-02-08 21:21:28 +0200

    adding styling in UserProfile responsive

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,8 +3,8 @@ function UserProfile() {
   return (
     <div className="user-profile bg-gray-100 sm:p-4 md:p-8 md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg ">
       <img src={profileImg} alt="User" className="rounded-full mx-auto w-24 h-24 md:w-36 md:h-36" />
-      <h1 className="text-xl text-blue-800 my-4 sm:text-sm md:text-base">John Doe</h1>
-      <p className="text-gray-600 text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <h1 className="md:text-xl text-blue-800 my-4 sm:text-lg md:text-base">John Doe</h1>
+      <p className="text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
